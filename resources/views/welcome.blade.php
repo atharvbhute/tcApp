819701fb911c1f8eef675c1758b92d1bafed8499
atharv bhute <atharv.bhute18@gmail.com>
2017-03-05 02:44:54 +0530

    tc app initialized

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <ul class="list-inline">
+                    <li><a class="btn btn-block btn-primary" href="#">People</a></li>
+                    <li><a class="btn btn-block btn-primary" href="#">Competitions</a></li>
+                    <li><a class="btn btn-block btn-primary" href="#">News</a></li>
+                </ul>
+                <p>this is the landing page of the app</p>
+            </div>
+        </div>
+    </div>
+@endsection
